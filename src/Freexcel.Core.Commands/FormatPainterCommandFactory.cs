64478b30c0d68aaa7098b3ba637fd7db443661d3
tony-xmelon <tony.xmelon@gmail.com>
2026-05-19 14:29:09 +0300

    feat: finish responsive ribbon and format painter parity

--- src/Freexcel.Core.Commands/FormatPainterCommandFactory.cs
+++ src/Freexcel.Core.Commands/FormatPainterCommandFactory.cs
@@ -9,12 +9,36 @@ public static class FormatPainterCommandFactory
         Sheet sourceSheet,
         CellAddress sourceAddress,
         GridRange targetRange)
+        => Create(workbook, sourceSheet, new GridRange(sourceAddress, sourceAddress), targetRange);
+
+    public static IWorkbookCommand Create(
+        Workbook workbook,
+        Sheet sourceSheet,
+        GridRange sourceRange,
+        GridRange targetRange)
     {
-        var styleId = sourceSheet.GetCell(sourceAddress)?.StyleId
-            ?? sourceSheet.GetStyleOnly(sourceAddress.Row, sourceAddress.Col)
-            ?? StyleId.Default;
-        var sourceStyle = workbook.GetStyle(styleId);
-        return new ApplyStyleCommand(targetRange.Start.Sheet, targetRange, StyleDiff.FromStyle(sourceStyle));
+        if (sourceRange.RowCount == 1 && sourceRange.ColCount == 1)
+        {
+            var styleId = GetSourceStyleId(sourceSheet, sourceRange.Start);
+            var sourceStyle = workbook.GetStyle(styleId);
+            return new ApplyStyleCommand(targetRange.Start.Sheet, targetRange, StyleDiff.FromStyle(sourceStyle));
+        }
+
+        var commands = new List<IWorkbookCommand>();
+        foreach (var targetAddress in targetRange.AllCells())
+        {
+            var sourceAddress = new CellAddress(
+                sourceSheet.Id,
+                sourceRange.Start.Row + ((targetAddress.Row - targetRange.Start.Row) % sourceRange.RowCount),
+                sourceRange.Start.Col + ((targetAddress.Col - targetRange.Start.Col) % sourceRange.ColCount));
+            var sourceStyle = workbook.GetStyle(GetSourceStyleId(sourceSheet, sourceAddress));
+            commands.Add(new ApplyStyleCommand(
+                targetRange.Start.Sheet,
+                new GridRange(targetAddress, targetAddress),
+                StyleDiff.FromStyle(sourceStyle)));
+        }
+
+        return new CompositeWorkbookCommand("Format Painter", commands);
     }
 
     public static IWorkbookCommand Create(
@@ -25,4 +49,9 @@ public static class FormatPainterCommandFactory
         var sourceStyle = workbook.GetStyle(sourceStyleId);
         return new ApplyStyleCommand(targetRange.Start.Sheet, targetRange, StyleDiff.FromStyle(sourceStyle));
     }
+
+    private static StyleId GetSourceStyleId(Sheet sourceSheet, CellAddress sourceAddress) =>
+        sourceSheet.GetCell(sourceAddress)?.StyleId
+        ?? sourceSheet.GetStyleOnly(sourceAddress.Row, sourceAddress.Col)
+        ?? StyleId.Default;
 }

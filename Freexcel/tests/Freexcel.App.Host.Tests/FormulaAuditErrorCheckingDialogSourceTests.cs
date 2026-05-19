fcc9ee31a50668757087502f0c2981e1c8366194
tony-xmelon <tony.xmelon@gmail.com>
2026-05-19 20:20:10 +0300

    Fix error checking rule persistence

---
+++ Freexcel/tests/Freexcel.App.Host.Tests/FormulaAuditErrorCheckingDialogSourceTests.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using FluentAssertions;
+
+namespace Freexcel.App.Host.Tests;
+
+public sealed class FormulaAuditErrorCheckingDialogSourceTests
+{
+    [Fact]
+    public void IgnoreSelected_RemovesAllIssuesForIgnoredCell()
+    {
+        var source = File.ReadAllText(WorkspaceFileLocator.Find("src", "Freexcel.App.Host", "ErrorCheckingDialog.cs"));
+
+        source.Should().Contain("var sameCellIssues = _issues");
+        source.Should().Contain(".Where(candidate =>");
+        source.Should().Contain("candidate.SheetId == issue.SheetId");
+        source.Should().Contain("candidate.Address.Equals(issue.Address)");
+        source.Should().Contain("foreach (var sameCellIssue in sameCellIssues)");
+        source.Should().Contain("_issues.Remove(sameCellIssue)");
+    }
+}

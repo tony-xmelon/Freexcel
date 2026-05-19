6347bbbf607e846824e902cc72b4d2a19e81230f
tony-xmelon <tony.xmelon@gmail.com>
2026-05-19 13:59:50 +0300

    Extract worksheet data navigation planner

--- Freexcel/src/Freexcel.App.Host/ExcelWorksheetNavigationPlanner.cs
+++ Freexcel/src/Freexcel.App.Host/ExcelWorksheetNavigationPlanner.cs
@@ -43,4 +43,77 @@ public static class ExcelWorksheetNavigationPlanner
             _ => null
         };
     }
+
+    public static CellAddress FindVerticalDataBoundary(Sheet? sheet, CellAddress current, int rowDirection)
+    {
+        var startFull = CellHasData(sheet, current.Row, current.Col);
+        var row = current.Row;
+        while (true)
+        {
+            var next = (long)row + rowDirection;
+            if (next is < 1 or > CellAddress.MaxRow)
+                break;
+
+            var nextRow = (uint)next;
+            var nextFull = CellHasData(sheet, nextRow, current.Col);
+            if (startFull && !nextFull)
+                break;
+
+            row = nextRow;
+            if (!startFull && nextFull)
+                break;
+        }
+
+        return new CellAddress(current.Sheet, row, current.Col);
+    }
+
+    public static CellAddress FindHorizontalDataBoundary(Sheet? sheet, CellAddress current, int columnDirection)
+    {
+        var startFull = CellHasData(sheet, current.Row, current.Col);
+        var column = current.Col;
+        while (true)
+        {
+            var next = (long)column + columnDirection;
+            if (next is < 1 or > CellAddress.MaxCol)
+                break;
+
+            var nextColumn = (uint)next;
+            var nextFull = CellHasData(sheet, current.Row, nextColumn);
+            if (startFull && !nextFull)
+                break;
+
+            column = nextColumn;
+            if (!startFull && nextFull)
+                break;
+        }
+
+        return new CellAddress(current.Sheet, current.Row, column);
+    }
+
+    public static CellAddress GetCtrlEndCell(Sheet? sheet, SheetId sheetId)
+    {
+        var maxRow = 1u;
+        var maxCol = 1u;
+        if (sheet is not null)
+        {
+            foreach (var (address, _) in sheet.GetUsedCells())
+            {
+                if (address.Row > maxRow)
+                    maxRow = address.Row;
+                if (address.Col > maxCol)
+                    maxCol = address.Col;
+            }
+        }
+
+        return new CellAddress(sheetId, maxRow, maxCol);
+    }
+
+    private static bool CellHasData(Sheet? sheet, uint row, uint col)
+    {
+        if (sheet is null)
+            return false;
+
+        var value = sheet.GetValue(new CellAddress(sheet.Id, row, col));
+        return value is not null and not BlankValue;
+    }
 }

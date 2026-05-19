8fbb1f4539ccfd82d07a448d5f6b5e0c155c6fc1
tony-xmelon <tony.xmelon@gmail.com>
2026-05-19 12:52:38 +0300

    fix format painter persistence and ranges

--- tests/Freexcel.App.Host.Tests/MainWindowSourceHygieneTests.cs
+++ tests/Freexcel.App.Host.Tests/MainWindowSourceHygieneTests.cs
@@ -15,14 +15,31 @@ public sealed class MainWindowSourceHygieneTests
     }
 
     [Fact]
-    public void PersistentFormatPainter_HasDedicatedStateHandlerAndCancelHelper()
+    public void PersistentFormatPainter_UsesPreviewMouseDownSoButtonDoubleClickCannotBeOverwrittenByClick()
     {
         var source = File.ReadAllText(WorkspaceFileLocator.Find("src", "Freexcel.App.Host", "MainWindow.xaml.cs"));
         var xaml = File.ReadAllText(WorkspaceFileLocator.Find("src", "Freexcel.App.Host", "MainWindow.xaml"));
 
         source.Should().Contain("private bool _formatPainterPersistent;");
-        source.Should().Contain("FormatPainterBtn_MouseDoubleClick");
+        source.Should().Contain("FormatPainterBtn_PreviewMouseLeftButtonDown");
+        source.Should().Contain("if (e.ClickCount != 2) return;");
+        source.Should().Contain("CaptureFormatPainterSource(persistent: true);");
+        source.Should().Contain("e.Handled = true;");
         source.Should().Contain("CancelFormatPainter");
-        xaml.Should().Contain("MouseDoubleClick=\"FormatPainterBtn_MouseDoubleClick\"");
+        xaml.Should().Contain("PreviewMouseLeftButtonDown=\"FormatPainterBtn_PreviewMouseLeftButtonDown\"");
+        xaml.Should().NotContain("MouseDoubleClick=\"FormatPainterBtn_MouseDoubleClick\"");
+    }
+
+    [Fact]
+    public void FormatPainterApplication_UsesTargetSelectionRangeWhenAvailable()
+    {
+        var source = File.ReadAllText(WorkspaceFileLocator.Find("src", "Freexcel.App.Host", "MainWindow.xaml.cs"));
+
+        source.Should().Contain("private bool _formatPainterTargetSelectionActive;");
+        source.Should().Contain("TryApplyFormatPainter(GridRange targetRange)");
+        source.Should().Contain("SheetGrid.SelectedRange is { } selectedRange");
+        source.Should().Contain("selectedRange.Contains(newAddr)");
+        source.Should().Contain("TryApplyFormatPainter(selectedRange)");
+        source.Should().NotContain("var targetRange = new GridRange(addr, addr);");
     }
 }

bb7c3961c50fa64ea61523f70b28b3e80689819b
tony-xmelon <tony.xmelon@gmail.com>
2026-05-21 20:00:04 +0300

    feat: bind unqualified this row structured references

--- Freexcel/tests/Freexcel.Core.Calc.Tests/StructuredReferenceCurrentRowTests.cs
+++ Freexcel/tests/Freexcel.Core.Calc.Tests/StructuredReferenceCurrentRowTests.cs
@@ -112,4 +112,47 @@ public sealed class StructuredReferenceCurrentRowTests
         report.RecalculatedCells.Should().NotContain(d2);
         sheet.GetValue(d3).Should().Be(new NumberValue(23));
     }
+
+    [Fact]
+    public void Recalculate_UnqualifiedThisRowStructuredReference_UsesContainingTableRow()
+    {
+        var workbook = new Workbook("StructuredReferenceUnqualifiedThisRowTest");
+        var sheet = workbook.AddSheet("Data");
+        var engine = new RecalcEngine(new DependencyGraph(), new FormulaEvaluator());
+
+        sheet.SetCell(new CellAddress(sheet.Id, 1, 1), new TextValue("Amount"));
+        sheet.SetCell(new CellAddress(sheet.Id, 1, 2), new TextValue("Tax"));
+        sheet.SetCell(new CellAddress(sheet.Id, 1, 3), new TextValue("Total"));
+        sheet.SetCell(new CellAddress(sheet.Id, 2, 1), new NumberValue(10));
+        sheet.SetCell(new CellAddress(sheet.Id, 2, 2), new NumberValue(1));
+        sheet.SetCell(new CellAddress(sheet.Id, 3, 1), new NumberValue(20));
+        sheet.SetCell(new CellAddress(sheet.Id, 3, 2), new NumberValue(2));
+
+        var table = new StructuredTableModel
+        {
+            Id = 1,
+            Name = "Sales",
+            DisplayName = "Sales",
+            Range = new GridRange(
+                new CellAddress(sheet.Id, 1, 1),
+                new CellAddress(sheet.Id, 3, 3)),
+            HasAutoFilter = true,
+            StyleName = "TableStyleMedium2",
+            ShowRowStripes = true
+        };
+        table.Columns.Add(new StructuredTableColumnModel(1, "Amount"));
+        table.Columns.Add(new StructuredTableColumnModel(2, "Tax"));
+        table.Columns.Add(new StructuredTableColumnModel(3, "Total"));
+        sheet.StructuredTables.Add(table);
+
+        var c2 = new CellAddress(sheet.Id, 2, 3);
+        var c3 = new CellAddress(sheet.Id, 3, 3);
+        sheet.SetFormula(c2, "SUM([[#This Row],[Amount]:[Tax]])");
+        sheet.SetFormula(c3, "SUM([[#This Row],[Amount]:[Tax]])");
+
+        engine.RecalculateAllFormulas(workbook);
+
+        sheet.GetValue(c2).Should().Be(new NumberValue(11));
+        sheet.GetValue(c3).Should().Be(new NumberValue(22));
+    }
 }

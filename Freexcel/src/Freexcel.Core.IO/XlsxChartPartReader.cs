c7bd0f4def4509ed72cf07537f2a57f3e4a23505
tony-xmelon <tony.xmelon@gmail.com>
2026-05-21 14:25:02 +0300

    Extract XLSX chart formatting reader

--- Freexcel/src/Freexcel.Core.IO/XlsxChartPartReader.cs
+++ Freexcel/src/Freexcel.Core.IO/XlsxChartPartReader.cs
@@ -938,12 +938,12 @@ public static class XlsxChartPartReader
     private static void ApplyChartLevelProperties(XDocument chartXml, ChartModel chart)
     {
         var chartElement = chartXml.Root?.Element(ChartNs + "chart");
-        ApplyChartTitleFormatting(chartElement?.Element(ChartNs + "title"), chart);
-        ApplyChartAreaShapeProperties(chartXml.Root?.Element(ChartNs + "spPr"), chart);
+        XlsxChartFormattingReader.ApplyChartTitleFormatting(chartElement?.Element(ChartNs + "title"), chart);
+        XlsxChartFormattingReader.ApplyChartAreaShapeProperties(chartXml.Root?.Element(ChartNs + "spPr"), chart);
         var plotArea = chartElement?.Element(ChartNs + "plotArea");
         chart.PlotAreaLayout = XlsxChartMetadataReader.ReadManualLayout(plotArea?.Element(ChartNs + "layout"));
         chart.DataTable = ReadChartDataTable(plotArea?.Element(ChartNs + "dTable"));
-        ApplyPlotAreaShapeProperties(plotArea?.Element(ChartNs + "spPr"), chart);
+        XlsxChartFormattingReader.ApplyPlotAreaShapeProperties(plotArea?.Element(ChartNs + "spPr"), chart);
         XlsxChartAxisReader.ApplyAxisMetadata(plotArea, chart);
         XlsxChartDataLabelReader.ApplyDataLabels(plotArea, chart);
 
@@ -1044,86 +1044,6 @@ public static class XlsxChartPartReader
         {
             chart.LegendTextColor = textColor;
             chart.LegendTextThemeColor = null;
-        }
-    }
-
-    private static void ApplyChartTitleFormatting(XElement? titleElement, ChartModel chart)
-    {
-        var runProperties = titleElement?
-            .Descendants(DrawingNs + "rPr")
-            .FirstOrDefault();
-        if (runProperties is null)
-            return;
-
-        if (int.TryParse(runProperties.Attribute("sz")?.Value, out var size))
-            chart.ChartTitleFontSize = Math.Clamp(size / 100.0, 6, 72);
-
-        var solidFill = runProperties.Element(DrawingNs + "solidFill");
-        if (solidFill is not null && XlsxDrawingColorReader.TryReadConcreteColor(solidFill, DrawingNs, out var color))
-            chart.ChartTitleTextColor = color;
-    }
-
-    private static void ApplyChartAreaShapeProperties(XElement? shapeProperties, ChartModel chart)
-    {
-        if (shapeProperties is null)
-            return;
-
-        var solidFill = shapeProperties.Element(DrawingNs + "solidFill");
-        if (solidFill is null)
-            return;
-
-        if (XlsxDrawingColorReader.TryReadThemeColorReference(solidFill, DrawingNs, out var themeColor))
-        {
-            chart.ChartAreaFillThemeColor = themeColor;
-            chart.ChartAreaFillColor = null;
-        }
-        else if (XlsxDrawingColorReader.TryReadConcreteColor(solidFill, DrawingNs, out var color))
-        {
-            chart.ChartAreaFillColor = color;
-            chart.ChartAreaFillThemeColor = null;
-        }
-    }
-
-    private static void ApplyPlotAreaShapeProperties(XElement? shapeProperties, ChartModel chart)
-    {
-        if (shapeProperties is null)
-            return;
-
-        var solidFill = shapeProperties.Element(DrawingNs + "solidFill");
-        if (solidFill is not null)
-        {
-            if (XlsxDrawingColorReader.TryReadThemeColorReference(solidFill, DrawingNs, out var themeColor))
-            {
-                chart.PlotAreaFillThemeColor = themeColor;
-                chart.PlotAreaFillColor = null;
-            }
-            else if (XlsxDrawingColorReader.TryReadConcreteColor(solidFill, DrawingNs, out var color))
-            {
-                chart.PlotAreaFillColor = color;
-                chart.PlotAreaFillThemeColor = null;
-            }
-        }
-
-        var line = shapeProperties.Element(DrawingNs + "ln");
-        if (line is null)
-            return;
-
-        if (int.TryParse(line.Attribute("w")?.Value, out var emus))
-            chart.PlotAreaBorderThickness = Math.Clamp(emus / 12700.0, 0, 10);
-
-        var lineFill = line.Element(DrawingNs + "solidFill");
-        if (lineFill is null)
-            return;
-
-        if (XlsxDrawingColorReader.TryReadThemeColorReference(lineFill, DrawingNs, out var borderThemeColor))
-        {
-            chart.PlotAreaBorderThemeColor = borderThemeColor;
-            chart.PlotAreaBorderColor = null;
-        }
-        else if (XlsxDrawingColorReader.TryReadConcreteColor(lineFill, DrawingNs, out var borderColor))
-        {
-            chart.PlotAreaBorderColor = borderColor;
-            chart.PlotAreaBorderThemeColor = null;
         }
     }
 

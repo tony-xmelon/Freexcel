b02001c7543b63e12be2b66f068a817ca2c9b139
tony-xmelon <tony.xmelon@gmail.com>
2026-05-23 19:21:56 +0300

    Disable consolidate delete without selection

--- Freexcel/tests/Freexcel.App.Host.Tests/DataToolDialogTests.cs
+++ Freexcel/tests/Freexcel.App.Host.Tests/DataToolDialogTests.cs
@@ -655,6 +655,9 @@ public sealed class DataToolDialogTests
         source.Should().Contain("Use _labels in:");
         source.Should().Contain("Content = \"_Add\"");
         source.Should().Contain("Content = \"_Delete\"");
+        source.Should().Contain("_deleteReferenceButton");
+        source.Should().Contain("UpdateReferenceButtons");
+        source.Should().Contain("_referencesList.SelectionChanged");
         source.Should().Contain("AddReferenceButton_Click");
         source.Should().Contain("DeleteReferenceButton_Click");
         source.Should().Contain("CreateReferenceEditor(_referenceBox");

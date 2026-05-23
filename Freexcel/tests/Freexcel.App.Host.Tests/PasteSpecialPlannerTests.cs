0b260313e8efe3acf59ee7eafc90f623430dbe5a
tony-xmelon <tony.xmelon@gmail.com>
2026-05-23 12:13:20 +0300

    Preserve paste special source theme option

--- Freexcel/tests/Freexcel.App.Host.Tests/PasteSpecialPlannerTests.cs
+++ Freexcel/tests/Freexcel.App.Host.Tests/PasteSpecialPlannerTests.cs
@@ -24,6 +24,7 @@ public sealed class PasteSpecialPlannerTests
 
     [Theory]
     [InlineData(PasteSpecialDialogMode.AllExceptBorders, PasteSpecialContentKind.AllExceptBorders)]
+    [InlineData(PasteSpecialDialogMode.AllUsingSourceTheme, PasteSpecialContentKind.AllUsingSourceTheme)]
     [InlineData(PasteSpecialDialogMode.AllMergingConditionalFormats, PasteSpecialContentKind.AllMergingConditionalFormats)]
     [InlineData(PasteSpecialDialogMode.FormulasAndNumberFormats, PasteSpecialContentKind.FormulasAndNumberFormats)]
     [InlineData(PasteSpecialDialogMode.ValuesAndNumberFormats, PasteSpecialContentKind.ValuesAndNumberFormats)]

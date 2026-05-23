8e2151843ca3227bc5875e948b5b9af7c60cec73
tony-xmelon <tony.xmelon@gmail.com>
2026-05-23 18:57:50 +0300

    Split GridView state helpers

--- Freexcel/src/Freexcel.App.UI/GridView.cs
+++ Freexcel/src/Freexcel.App.UI/GridView.cs
@@ -1,7 +1,6 @@
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Threading;
 using System.Globalization;
 using Freexcel.Core.Model;
 using CellHAlign  = Freexcel.Core.Model.HorizontalAlignment;
@@ -192,54 +191,7 @@ public partial class GridView : FrameworkElement
     }
 
 
-    private Dictionary<(uint Row, uint Col), GridRange> _mergeLookup = [];
-
-    private void RebuildMergeLookup()
-    {
-        _mergeLookup.Clear();
-        if (MergedRegions == null || Viewport == null) return;
-
-        var visRows = new HashSet<uint>(Viewport.RowMetrics.Select(r => r.Row));
-        var visCols = new HashSet<uint>(Viewport.ColMetrics.Select(c => c.Col));
-
-        foreach (var merge in MergedRegions)
-        {
-            for (uint r = merge.Start.Row; r <= merge.End.Row; r++)
-            {
-                if (!visRows.Contains(r)) continue;
-                for (uint c = merge.Start.Col; c <= merge.End.Col; c++)
-                {
-                    if (visCols.Contains(c))
-                        _mergeLookup[(r, c)] = merge;
-                }
-            }
-        }
-    }
-
     // â”€â”€ Marching ants â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-
-    private DispatcherTimer? _marchTimer;
-    private double _marchOffset;
-
-    private void StartMarchTimer()
-    {
-        if (_marchTimer != null) return;
-        _marchTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(80) };
-        _marchTimer.Tick += (_, _) =>
-        {
-            _marchOffset = (_marchOffset + 1.5) % 8.0;
-            InvalidateVisual();
-        };
-        _marchTimer.Start();
-    }
-
-    private void StopMarchTimer()
-    {
-        _marchTimer?.Stop();
-        _marchTimer = null;
-        _marchOffset = 0;
-        InvalidateVisual();
-    }
 
     // â”€â”€ Resize drag state â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
 

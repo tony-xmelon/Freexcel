2c6d641a6ba9a60145b033a78a3797322a0d208f
tony-xmelon <tony.xmelon@gmail.com>
2026-05-24 10:38:11 +0300

    Import ISO slash dates from text files

--- Freexcel/tests/Freexcel.Core.IO.Tests/DelimitedTextFileAdapterTests.cs
+++ Freexcel/tests/Freexcel.Core.IO.Tests/DelimitedTextFileAdapterTests.cs
@@ -96,6 +96,21 @@ public sealed class DelimitedTextFileAdapterTests
     }
 
     [Fact]
+    public void Load_UsesExcelLikeTextCoercionForIsoSlashDatesAndTimes()
+    {
+        var adapter = new DelimitedTextFileAdapter(".tsv", "Tab-separated values", '\t');
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes("2026/5/17\t2026/05/17 9:30\r\n"));
+
+        var workbook = adapter.Load(stream);
+        var sheet = workbook.Sheets.Single();
+
+        sheet.GetValue(new CellAddress(sheet.Id, 1, 1))
+            .Should().Be(DateTimeValue.FromDateTime(new DateTime(2026, 5, 17)));
+        sheet.GetValue(new CellAddress(sheet.Id, 1, 2))
+            .Should().Be(DateTimeValue.FromDateTime(new DateTime(2026, 5, 17, 9, 30, 0)));
+    }
+
+    [Fact]
     public void Load_UsesExcelLikeTextCoercionForIsoDateTimesWithSingleDigitHours()
     {
         var adapter = new DelimitedTextFileAdapter(".tsv", "Tab-separated values", '\t');

38c4324eb0d3e391d6508fe5fca917fd51a23d85
tony-xmelon <tony.xmelon@gmail.com>
2026-05-24 18:48:58 +0300

    Harden Excel open format alias coverage

--- tests/Freexcel.Core.IO.Tests/FileDialogFilterBuilderTests.cs
+++ tests/Freexcel.Core.IO.Tests/FileDialogFilterBuilderTests.cs
@@ -70,6 +70,33 @@ public sealed class FileDialogFilterBuilderTests
     }
 
     [Fact]
+    public void BuildOpenFilter_RealAdaptersExposeExcelOpenAliases()
+    {
+        var filter = FileDialogFilterBuilder.BuildOpenFilter(
+            [new XlsxFileAdapter(), new LegacyXlsFileAdapter(), new CsvFileAdapter()]);
+
+        filter.Should().Contain("*.xlsx;*.xlsm;*.xltx;*.xltm;*.xls;*.xlsb;*.xlt;*.csv");
+        filter.Should().Contain("Excel Binary Workbook (*.xlsb)|*.xlsb");
+        filter.Should().Contain("Excel 97-2003 Template (*.xlt)|*.xlt");
+        filter.Should().Contain("Excel Macro-Enabled Template (*.xltm)|*.xltm");
+    }
+
+    [Fact]
+    public void BuildSaveFilter_RealAdaptersExcludeOpenOnlyExcelFormats()
+    {
+        var filter = FileDialogFilterBuilder.BuildSaveFilter(
+            [new XlsxFileAdapter(), new LegacyXlsFileAdapter(), new CsvFileAdapter()]);
+
+        filter.Should().Be("Excel Workbook (*.xlsx)|*.xlsx|CSV (Comma-separated values) (*.csv)|*.csv");
+        filter.Should().NotContain("Excel Macro-Enabled Workbook (*.xlsm)|*.xlsm");
+        filter.Should().NotContain("Excel Template (*.xltx)|*.xltx");
+        filter.Should().NotContain("Excel Macro-Enabled Template (*.xltm)|*.xltm");
+        filter.Should().NotContain("Excel 97-2003 Workbook (*.xls)|*.xls");
+        filter.Should().NotContain("Excel Binary Workbook (*.xlsb)|*.xlsb");
+        filter.Should().NotContain("Excel 97-2003 Template (*.xlt)|*.xlt");
+    }
+
+    [Fact]
     public void FindOpenAdapter_ResolvesAliasesCaseInsensitively()
     {
         var adapter = new FakeAdapter([

e3eef3c39f7e25caa4c98f68575f7922e8786724
tony-xmelon <tony.xmelon@gmail.com>
2026-05-25 14:34:54 +0300

    Preview Quick Analysis with keyboard focus

--- tests/Freexcel.App.Host.Tests/QuickAnalysisHoverPreviewTests.cs
+++ tests/Freexcel.App.Host.Tests/QuickAnalysisHoverPreviewTests.cs
@@ -6,10 +6,16 @@ namespace Freexcel.App.Host.Tests;
 public sealed class QuickAnalysisHoverPreviewTests
 {
     [Fact]
-    public void QuickAnalysisHover_SetsAndClearsGridPreviewRange()
+    public void QuickAnalysisHoverAndKeyboardFocus_SetAndClearGridPreviewRange()
     {
         var source = File.ReadAllText(WorkspaceFileLocator.Find("src", "Freexcel.App.Host", "MainWindow.QuickAnalysis.cs"));
 
+        source.Should().Contain("item.GotKeyboardFocus += QuickAnalysisMenuItem_GotKeyboardFocus;");
+        source.Should().Contain("item.LostKeyboardFocus += QuickAnalysisMenuItem_LostKeyboardFocus;");
+        source.Should().Contain("private void QuickAnalysisMenuItem_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)");
+        source.Should().Contain("private void QuickAnalysisMenuItem_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)");
+        source.Should().Contain("ShowQuickAnalysisPreview(sender);");
+        source.Should().Contain("ClearQuickAnalysisPreview();");
         source.Should().Contain("SheetGrid.QuickAnalysisPreviewRange = preview.Range");
         source.Should().Contain("SheetGrid.QuickAnalysisPreviewRange = null");
     }

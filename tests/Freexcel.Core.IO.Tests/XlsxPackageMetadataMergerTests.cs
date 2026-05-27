21dfedf6d458c87e16b5ac8c3c0c676db404bc12
tony-xmelon <tony.xmelon@gmail.com>
2026-05-27 23:55:34 +0300

    Normalize XLSX relationship targets

--- tests/Freexcel.Core.IO.Tests/XlsxPackageMetadataMergerTests.cs
+++ tests/Freexcel.Core.IO.Tests/XlsxPackageMetadataMergerTests.cs
@@ -127,6 +127,30 @@ public sealed class XlsxPackageMetadataMergerTests
     }
 
     [Fact]
+    public void MergeRelationshipParts_PreservesWhitespacePaddedInternalTargetsForCopiedParts()
+    {
+        using var sourcePackage = CreatePackageWithWhitespacePaddedInternalMediaRelationship();
+        using var targetPackage = CreatePackageWithExistingWorksheetRelationships();
+        using var sourceArchive = new ZipArchive(sourcePackage, ZipArchiveMode.Read, leaveOpen: true);
+        using var targetArchive = new ZipArchive(targetPackage, ZipArchiveMode.Update, leaveOpen: true);
+
+        var generatedEntriesBeforeMerge = XlsxPackageMetadataMerger.CopyUnknownPackageParts(sourceArchive, targetArchive);
+        XlsxPackageMetadataMerger.MergeRelationshipParts(sourceArchive, targetArchive, generatedEntriesBeforeMerge);
+
+        targetArchive.GetEntry("xl/media/image 1.png").Should().NotBeNull();
+
+        var relsXml = LoadXml(targetArchive.GetEntry("xl/worksheets/_rels/sheet1.xml.rels")!);
+        XNamespace relationshipNs = "http://schemas.openxmlformats.org/package/2006/relationships";
+        relsXml.Root!
+            .Elements(relationshipNs + "Relationship")
+            .Where(element =>
+                element.Attribute("Type")?.Value == "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" &&
+                element.Attribute("Target")?.Value == " ../media/image%201.png ")
+            .Should()
+            .ContainSingle();
+    }
+
+    [Fact]
     public void MergeRelationshipParts_PreservesExternalTargetsWithoutPackageEntriesAndRemapsIds()
     {
         using var sourcePackage = CreatePackageWithExternalWorksheetRelationship();
@@ -303,6 +327,34 @@ public sealed class XlsxPackageMetadataMergerTests
                   <Relationship Id="rIdImage"
                                 Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image"
                                 Target="../media/image%201.png"/>
+                </Relationships>
+                """);
+            var mediaEntry = archive.CreateEntry("xl/media/image 1.png");
+            using var mediaStream = mediaEntry.Open();
+            mediaStream.Write([0x89, 0x50, 0x4E, 0x47]);
+        }
+
+        package.Position = 0;
+        return package;
+    }
+
+    private static MemoryStream CreatePackageWithWhitespacePaddedInternalMediaRelationship()
+    {
+        var package = new MemoryStream();
+        using (var archive = new ZipArchive(package, ZipArchiveMode.Create, leaveOpen: true))
+        {
+            WritePackageEntry(archive, "[Content_Types].xml", """
+                <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+                  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+                  <Default Extension="xml" ContentType="application/xml"/>
+                  <Default Extension="png" ContentType="image/png"/>
+                </Types>
+                """);
+            WritePackageEntry(archive, "xl/worksheets/_rels/sheet1.xml.rels", """
+                <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+                  <Relationship Id="rIdImage"
+                                Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image"
+                                Target=" ../media/image%201.png "/>
                 </Relationships>
                 """);
             var mediaEntry = archive.CreateEntry("xl/media/image 1.png");

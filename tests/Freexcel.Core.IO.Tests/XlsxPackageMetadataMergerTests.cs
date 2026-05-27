a6ecd4026b94e78e1f6d7a087ee8a895e24bc404
tony-xmelon <tony.xmelon@gmail.com>
2026-05-27 16:36:42 +0300

    Preserve percent-encoded XLSX relationship targets

---
+++ tests/Freexcel.Core.IO.Tests/XlsxPackageMetadataMergerTests.cs
@@ -0,0 +1,99 @@
+using System.IO.Compression;
+using System.Xml.Linq;
+using FluentAssertions;
+using Freexcel.Core.IO;
+
+namespace Freexcel.Core.IO.Tests;
+
+public sealed class XlsxPackageMetadataMergerTests
+{
+    [Fact]
+    public void MergeRelationshipParts_PreservesPercentEncodedInternalTargetsForCopiedParts()
+    {
+        using var sourcePackage = CreatePackageWithPercentEncodedMediaRelationship();
+        using var targetPackage = CreatePackageWithExistingWorksheetRelationships();
+        using var sourceArchive = new ZipArchive(sourcePackage, ZipArchiveMode.Read, leaveOpen: true);
+        using var targetArchive = new ZipArchive(targetPackage, ZipArchiveMode.Update, leaveOpen: true);
+
+        var generatedEntriesBeforeMerge = XlsxPackageMetadataMerger.CopyUnknownPackageParts(sourceArchive, targetArchive);
+        XlsxPackageMetadataMerger.MergeRelationshipParts(sourceArchive, targetArchive, generatedEntriesBeforeMerge);
+
+        targetArchive.GetEntry("xl/media/image 1.png").Should().NotBeNull();
+
+        var relsXml = LoadXml(targetArchive.GetEntry("xl/worksheets/_rels/sheet1.xml.rels")!);
+        XNamespace relationshipNs = "http://schemas.openxmlformats.org/package/2006/relationships";
+        relsXml.Root!
+            .Elements(relationshipNs + "Relationship")
+            .Where(element =>
+                element.Attribute("Type")?.Value == "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" &&
+                element.Attribute("Target")?.Value == "../media/image%201.png")
+            .Should()
+            .ContainSingle();
+    }
+
+    private static MemoryStream CreatePackageWithPercentEncodedMediaRelationship()
+    {
+        var package = new MemoryStream();
+        using (var archive = new ZipArchive(package, ZipArchiveMode.Create, leaveOpen: true))
+        {
+            WritePackageEntry(archive, "[Content_Types].xml", """
+                <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+                  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+                  <Default Extension="xml" ContentType="application/xml"/>
+                  <Default Extension="png" ContentType="image/png"/>
+                </Types>
+                """);
+            WritePackageEntry(archive, "xl/worksheets/_rels/sheet1.xml.rels", """
+                <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+                  <Relationship Id="rIdImage"
+                                Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image"
+                                Target="../media/image%201.png"/>
+                </Relationships>
+                """);
+            var mediaEntry = archive.CreateEntry("xl/media/image 1.png");
+            using var mediaStream = mediaEntry.Open();
+            mediaStream.Write([0x89, 0x50, 0x4E, 0x47]);
+        }
+
+        package.Position = 0;
+        return package;
+    }
+
+    private static MemoryStream CreatePackageWithExistingWorksheetRelationships()
+    {
+        var package = new MemoryStream();
+        using (var archive = new ZipArchive(package, ZipArchiveMode.Create, leaveOpen: true))
+        {
+            WritePackageEntry(archive, "[Content_Types].xml", """
+                <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+                  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+                  <Default Extension="xml" ContentType="application/xml"/>
+                </Types>
+                """);
+            WritePackageEntry(archive, "xl/worksheets/_rels/sheet1.xml.rels", """
+                <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+                  <Relationship Id="rIdHyperlink"
+                                Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink"
+                                Target="https://example.com/docs"
+                                TargetMode="External"/>
+                </Relationships>
+                """);
+        }
+
+        package.Position = 0;
+        return package;
+    }
+
+    private static void WritePackageEntry(ZipArchive archive, string path, string content)
+    {
+        var entry = archive.CreateEntry(path);
+        using var writer = new StreamWriter(entry.Open());
+        writer.Write(content);
+    }
+
+    private static XDocument LoadXml(ZipArchiveEntry entry)
+    {
+        using var stream = entry.Open();
+        return XDocument.Load(stream);
+    }
+}

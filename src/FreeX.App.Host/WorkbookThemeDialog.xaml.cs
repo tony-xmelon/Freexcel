70a7329b713005ae5b3ef0f5c805982ed773d406
tony-xmelon <tony.xmelon@gmail.com>
2026-05-30 12:35:00 +0300

    Label workbook theme color fields

--- src/FreeX.App.Host/WorkbookThemeDialog.xaml.cs
+++ src/FreeX.App.Host/WorkbookThemeDialog.xaml.cs
@@ -16,6 +16,7 @@ public partial class WorkbookThemeDialog : Window
 
         _initialTheme = theme;
         InitializeComponent();
+        ApplyThemeColorAutomationMetadata();
         PopulateOptions();
         WirePreviewRefresh();
         LoadTheme(theme);
